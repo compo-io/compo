57bfd40080ebc5cb74b795309c63f9c5d3c55d6b
simon <even.simon@gmail.com>
2018-07-09 14:58:06 +0200

    added types

---
+++ lib/index.ts
@@ -0,0 +1,15 @@
+import {AuthFile} from "./AuthFile";
+import {PackageFile} from "./PackageFile";
+import {CompoFile} from "./CompoFile";
+import {Builder} from './Builder';
+import {User} from "./User";
+import {Logger} from "./Logger";
+
+export = {
+    AuthFile,
+    PackageFile,
+    CompoFile,
+    Builder,
+    User,
+    Logger,
+}
